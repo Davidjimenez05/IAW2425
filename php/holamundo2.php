941cad723813ce658b708591bbdd460ede84b499
Davidjimenez05 <davidjimenez05@iesamachado.org>
2024-11-28 14:27:31 +0100

    subido php

---
+++ php/holamundo2.php
@@ -0,0 +1,8 @@
+<?php
+	
+    $cadena1 = "<p>Hola mundo ";
+    $cadena2 = "desde PHP</p>";
+
+    echo $cadena1 . $cadena2; //concatenamos las dos cadenas o tambén $cadena1$cadena2
+
+	?>
---
+++ php/operadoresNumericos.php
@@ -0,0 +1,18 @@
+<?php
+	
+$a = 47;
+$b = -3;
+
+echo "Suma: " . ($a + $b) . "<br>";
+
+echo "Resta: " . ($a - $b) . "<br>";
+
+echo "Dividir: " . ($a / $b) . "<br>";
+
+echo "Multiplicar: " . ($a * $b) . "<br>";
+
+echo "Potencia: " . ($a ** $b) . "<br>";
+
+echo "Resto: " . ($a % $b) . "<br>";
+
+?>
